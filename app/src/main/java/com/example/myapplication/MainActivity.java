78e80e39c0a62c8d9604c23934cc16353673f238
Bayu-F <bayuronald@hotmail.com>
2023-04-28 18:53:55 +0200

    added cancel button and counter for login attempts

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,30 +27,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialButton loginBtn, cancelBtn;
+    EditText username,password;
+    TextView attemptsLeft, attemptsLeftCounter;
+    int counter = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login_page);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        attemptsLeftCounter = findViewById(R.id.attempts_left_counter);
+        attemptsLeft = findViewById(R.id.attempts_left);
+        loginBtn = findViewById(R.id.loginbtn);
+        cancelBtn = findViewById(R.id.cancel_button);
+        attemptsLeft.setVisibility(View.GONE);
+        attemptsLeftCounter.setVisibility(View.GONE);
 
-        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.loginbtn);
 
         //admin and admin
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    //correct
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, "Redirecting...", Toast.LENGTH_SHORT).show();
                     Intent i = new Intent(MainActivity.this, MainActivity3.class);
                     startActivity(i);
-                } else
-                    //incorrect
-                    Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+                    attemptsLeft.setVisibility(View.VISIBLE);
+                    attemptsLeftCounter.setVisibility(View.VISIBLE);
+                    counter--;
+                    attemptsLeftCounter.setText(Integer.toString(counter));
+
+                    if(counter == 0){
+                        loginBtn.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
